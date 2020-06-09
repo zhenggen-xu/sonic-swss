3c87153ffe01dd24db8c50d36505df0807133726
Vasant <vapatil@linkedin.com>
2020-06-09 00:37:29 +0000

    Address code-review comments

--- tests/dvslib/dvs_vlan.py
+++ tests/dvslib/dvs_vlan.py
@@ -30,24 +30,6 @@ class DVSVlan(object):
         member = "Vlan{}|{}".format(vlan, interface)
         self.config_db.delete_entry("VLAN_MEMBER", member)
 
-    def create_port_channel(self, lag_id, admin_status="up", mtu="1500"):
-        lag = "PortChannel{}".format(lag_id)
-        lag_entry = {"admin_status": admin_status, "mtu": mtu}
-        self.config_db.create_entry("PORTCHANNEL", lag, lag_entry)
-
-    def remove_port_channel(self, lag_id):
-        lag = "PortChannel{}".format(lag_id)
-        self.config_db.delete_entry("PORTCHANNEL", lag)
-
-    def create_port_channel_member(self, lag_id, interface):
-        member = "PortChannel{}|{}".format(lag_id, interface)
-        member_entry = {"NULL": "NULL"}
-        self.config_db.create_entry("PORTCHANNEL_MEMBER", member, member_entry)
-
-    def remove_port_channel_member(self, lag_id, interface):
-        member = "PortChannel{}|{}".format(lag_id, interface)
-        self.config_db.delete_entry("PORTCHANNEL_MEMBER", member)
-
     def check_app_db_vlan_fields(self, fvs, admin_status="up", mtu="9100"):
         assert fvs.get("admin_status") == admin_status
         assert fvs.get("mtu") == mtu
